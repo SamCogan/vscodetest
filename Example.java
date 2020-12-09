f3f4914d2d819b0c38c83a5161dbee54943f8277
Cogee <cogansam@gmail.com>
2020-12-09 03:42:51 +0000

    second commit

--- Example.java
+++ Example.java
@@ -1,6 +1,6 @@
 import javax.swing.JOptionPane;
 public class Example{
     public static void main(String[] args){
-        JOptionPane.showMessageDialog(null,"Hello world!");
+        JOptionPane.showMessageDialog(null,"Hello world!!!!");     
     }
 }
